4807b1f5d9fa1edbb9145b19be608e776b9e4aa9
newton786241 <24tamseelanam11@gmail.com>
2025-02-01 20:46:59 +0530

    deploy fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Navbar from "@/Components/Navbar";
+import Navbar from "@/app/Components/Navbar";
 import styles from './style'
-import Hero from "@/Components/Hero";
-import Stats from "@/Components/Stats";
-import Business from "@/Components/Business";
-import Billing from "@/Components/Billing";
-import CardDeal from "@/Components/CardDeal";
-import Testimonials from "@/Components/Testimonials";
-import Clients from "@/Components/Clients";
-import CTA from "@/Components/CTA";
-import Footer from "@/Components/Footer";
+import Hero from "@/app/Components/Hero";
+import Stats from "@/app/Components/Stats";
+import Business from "@/app/Components/Business";
+import Billing from "@/app/Components/Billing";
+import CardDeal from "@/app/Components/CardDeal";
+import Testimonials from "@/app/Components/Testimonials";
+import Clients from "@/app/Components/Clients";
+import CTA from "@/app/Components/CTA";
+import Footer from "@/app/Components/Footer";
 
 export default function Home() {
   return (
