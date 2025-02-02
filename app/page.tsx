f8853358e051a862cad200a699528bc827315529
newton786241 <24tamseelanam11@gmail.com>
2025-02-02 14:43:48 +0530

    text fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Footer from "@/app/Components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-black w-full overflow-hidden">
+    <div className="bg-black w-full overflow-hidden text-white">
       <div className="sm:px-16 px-6 flex justify-center items-center">
         <div className="xl:max-w-[1280px] w-full">
           <Navbar/>
